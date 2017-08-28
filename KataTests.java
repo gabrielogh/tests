6e6ebd2bb4c1e5930b0f5598a6f4da23a0c6393a
Ozono Soft <LauryGaido>
2017-08-28 17:02:18 -0300

    clase mia

--- KataTests.java
+++ KataTests.java
@@ -1,4 +1,17 @@
+import java.util.Collection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class KataTests{
+	
+	    @Test
+        public final void testValuesOfKata(){
+        Kata fun = new multiple_of_five(10);
+        assertEquals("El resultado del test para mult 5 es: ", fun.multiple_of_five()[10]);
+        
+    }
 	
 	
 }
